14153ac3b3f3cbbca1ff0e11403729cfad8f87dd
mazenDev <mazenfathy841@gmail.com>
2026-08-15 20:25:48 +0300

    creating Publish route

--- app.js
+++ app.js
@@ -2,13 +2,10 @@ import express from "express";
 import {PORT} from "./config/env.js"
 import connectDB from "./database/mongodb.js";
 import dns from "node:dns";
+import Publish from "./routes/Publish.routes.js";
 dns.setServers(["8.8.8.8"]);
 const app = express();
-app.post("/", (req, res) => {
-  res.send({
-    mazen:"Nigga"
-  });
-});
+app.use("/publish",Publish)
 app.listen(PORT,async ()=>{
   console.log(`http://localhost:${PORT}`)
   await connectDB()
---
+++ routes/Publish.routes.js
@@ -0,0 +1,7 @@
+import express from "express"
+
+const Publish = express.Router()
+Publish.post('/',(req,res)=>{
+  
+})
+export default Publish;
