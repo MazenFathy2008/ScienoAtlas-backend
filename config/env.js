96fa23994c148a15e22c0287d61132009bea1a18
mazenDev <mazenfathy841@gmail.com>
2026-08-16 18:33:09 +0300

    sending token to the client

--- config/env.js
+++ config/env.js
@@ -1,4 +1,11 @@
 import { config } from "dotenv";
 const file = `.env.${process.env.NODE_ENV || "development"}.local`;
 config({ path: file });
-export const { PORT,MONGODB_URI,SUPABASE_URL,SUPABASE_SECRET_KEY } = process.env;
+export const {
+  PORT,
+  MONGODB_URI,
+  SUPABASE_URL,
+  SUPABASE_SECRET_KEY,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+} = process.env;
--- controllers/signUp.controller.js
+++ controllers/signUp.controller.js
@@ -1,6 +1,11 @@
 import User from "../models/user.model.js";
+import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { JWT_EXPIRES_IN, JWT_SECRET } from "../config/env.js";
 const signUpController = async (req, res, next) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
   try {
     if (!req.body.password) {
       const error = new Error("Password is required");
@@ -9,13 +14,29 @@ const signUpController = async (req, res, next) => {
     }
     const hashedPassword = await bcrypt.hash(req.body.password, 12);
     req.body.password = hashedPassword;
-    const user = new User({
-      ...req.body,
+    const user = await User.create([req.body], { session });
+    await session.commitTransaction();
+    const token = jwt.sign(
+      {
+        userId: user[0]._id,
+      },
+      JWT_SECRET,
+      { expiresIn: JWT_EXPIRES_IN },
+    );
+    res.status(201).json({
+      data: {
+        token,
+        username: user[0].username,
+        email: user[0].email,
+      },
+      success: true,
+      message: "User Signed Up successfully",
     });
-    await user.save();
-    res.status(201).json(req.body);
   } catch (err) {
+    await session.abortTransaction();
     next(err);
+  } finally {
+    await session.endSession();
   }
 };
 export { signUpController };
