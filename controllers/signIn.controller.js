59dae73426eb57fc20a37d5c58a25e782022a80b
mazenDev <mazenfathy841@gmail.com>
2026-08-18 13:54:00 +0300

    sending jwt as cookies from the backEnd to frontend

--- controllers/signIn.controller.js
+++ controllers/signIn.controller.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET,JWT_EXPIRES_IN } from "../config/env.js";
+import { JWT_SECRET, JWT_EXPIRES_IN } from "../config/env.js";
 const signInController = async (req, res, next) => {
   try {
     const { email, password } = req.body;
@@ -19,6 +19,12 @@ const signInController = async (req, res, next) => {
     }
     const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
       expiresIn: JWT_EXPIRES_IN,
+    });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      maxAge: 3 * 24 * 60 * 60 * 1000,
     });
     res.json({
       data: {
