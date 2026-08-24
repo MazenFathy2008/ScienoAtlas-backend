17e309d92fde556f43f17d0fe7951a97d6eda0ca
mazenDev <mazenfathy841@gmail.com>
2026-08-24 22:57:51 +0300

    adding time stamp

--- models/user.model.js
+++ models/user.model.js
@@ -1,47 +1,60 @@
 import mongoose from "mongoose";
+const userScheme = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 5,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minLength: 5,
+      lowercase: true,
+      match: [
+        /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+        "Please Enter a valid Email format",
+      ],
+    },
 
-const userScheme = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    minLenght: 5,
-    maxLenght: 50,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    minLenght: 5,
-    lowercase: true,
-    match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Please Enter a valid Email format"],
-  },
-  password: {
-    type: String,
-    required: true,
-    minLenght: 8,
-  },
-  age: {
-    type: Number,
-    required: true,
-  },
-  role: {
-    type: String,
-    required: true,
-    default: "user",
-  },
-  publisedPapers: {
-    type: Object,
-    required: true,
-    default: {
-      count: 0,
-      listOfPapers: [],
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+    },
+
+    age: {
+      type: Number,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      required: true,
+      default: "user",
+    },
+
+    publisedPapers: {
+      type: Object,
+      required: true,
+      default: {
+        count: 0,
+        listOfPapers: [],
+      },
+    },
+
+    reasonOfjoining: {
+      type: String,
     },
   },
-  reasonOfjoining: {
-    type: String,
+  {
+    timestamps: true,
   },
-});
+);
 const User = mongoose.model("User", userScheme);
+
 export default User;
